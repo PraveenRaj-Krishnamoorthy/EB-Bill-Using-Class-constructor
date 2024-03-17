cd5578d0bd13d54feb26c84a350099ecc7afd40a
PraveenRaj-Krishnamoorthy <praveenraj.krishnamoorthy@outlook.com>
2024-03-17 12:02:12 +0530

    Summary(Completed)
    Description(expected output received!)

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # EB Bill Using Class constructor
  EB Bill check app using class
+ EB - class constructor all data updated, output rinning fine;
---
+++ index.js
@@ -0,0 +1,41 @@
+class classes{
+    constructor(name,prev,curr,opt){
+        this.n = name;
+        this.p = prev;
+        this.c = curr;
+        this.o = opt;
+    }
+    condition(){
+        this.unit = this.c - this.p;
+        if (this.o==1) {
+            this.amount = this.unit*2;
+        } else {
+            this.amount = this.unit*4;
+        }
+        if (this.p>this.c) {
+            alert("Previous should be less than current!");
+            location.reload();
+        }
+    }
+    display(){
+        let output = document.querySelector(".show");
+        output.innerHTML=`
+        Name: ${this.n}<br>
+        Previous: ${this.p}<br>
+        Current: ${this.c}<br>
+        For: ${this.o==1?"Home":"Office"}<br>
+        Unit Used: ${this.unit}<br>
+        Amount: ${this.amount}`;
+    }
+}
+
+const func=()=>{
+    let form = document.forms["formData"];
+    let user = form["user"].value;
+    let pR = form["pR"].value;
+    let cR = form["cR"].value;
+    let opt = form["options"].value;
+    let eb = new classes(user,pR,cR,opt);
+    eb.condition();
+    eb.display();
+}
